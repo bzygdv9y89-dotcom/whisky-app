58c87f7fe34639c7f540a90137da801115010a03
bzygdv9y89-dotcom <bzygdv9y89@privaterelay.appleid.com>
2025-11-15 00:31:57 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const whiskyData = [
+  { id: 1, name: "Basil Hayden's", region: "Bourbon / Kentucky", short: "Light, approachable bourbon", abv: "40%" },
+  { id: 2, name: "Eagle Rare", region: "Bourbon / Kentucky", short: "Rich sweetness, caramel", abv: "45%" }
+  // add the rest of your whisky objects here (or paste the full list)
+];
